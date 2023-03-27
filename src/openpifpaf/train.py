95638f5e871aa2ff7947b2ec4e018fe05b5c3895
The-Hobbit-G <guojianting0616@163.com>
2023-03-27 18:47:21 +0200

    check datamodule type

--- src/openpifpaf/train.py
+++ src/openpifpaf/train.py
@@ -156,10 +156,12 @@ def main():
     
 
     datamodule = datasets.factory(args.dataset)
+    print(type(datamodule))
     if use_fpn:
         datamodule.use_fpn = use_fpn
         datamodule.head_stride = args.head_stride
         # datamodule.multiencoder_process()
+        # if type(datamodule) == 
 
     net_cpu, start_epoch = network.Factory().factory(head_metas=datamodule.head_metas)
     loss = network.losses.Factory().factory(datamodule.head_metas)
