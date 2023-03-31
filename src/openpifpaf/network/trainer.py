ffe2d3164d0cdfd058f445900f9b37f29b76f1a8
The-Hobbit-G <guojianting0616@163.com>
2023-03-31 16:35:16 +0200

    check the lengh of loss

--- src/openpifpaf/network/trainer.py
+++ src/openpifpaf/network/trainer.py
@@ -221,6 +221,7 @@ class Trainer():
                 #     print('target type: {}, output type: {}'.format(type(targets[i][2]),type(outputs[i][2])))
 
                 multistage_loss, multistage_head_losses = multi_apply(self.loss,outputs,targets)
+                print(len(targets) == len(outputs),len(multistage_loss))
             
                 # print('multistage_loss: {}, multistage_head_losses: {}'.format(multistage_loss,multistage_head_losses))
                 # average over the losses from different stage(could also do sum)
