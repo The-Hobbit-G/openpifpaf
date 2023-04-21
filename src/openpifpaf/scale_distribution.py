82c62e11690f59f00594b52b27bb50e988646716
The-Hobbit-G <guojianting0616@163.com>
2023-04-21 15:15:38 +0200

    remove main()

--- src/openpifpaf/scale_distribution.py
+++ src/openpifpaf/scale_distribution.py
@@ -17,55 +17,60 @@ import openpifpaf.datasets as datasets
 
 
 
-def main():
-    datamodule = datasets.factory('cocokp')
+# def main():
+datamodule = datasets.factory('cocokp')
 
-    print('square_edge: ', datamodule.square_edge)
-    print('extended_scale: ', datamodule.extended_scale)
-    print('orientation_invariant: ', datamodule.orientation_invariant)
-    print('upsample_stride: ', datamodule.upsample_stride)
+print('square_edge: ', datamodule.square_edge)
+print('extended_scale: ', datamodule.extended_scale)
+print('orientation_invariant: ', datamodule.orientation_invariant)
+print('upsample_stride: ', datamodule.upsample_stride)
 
-    # print(type(datamodule))
-    # print(len(train_loader))
-    datamodule.square_edge = 513
-    datamodule.extended_scale = False
-    datamodule.orientation_invariant = 0.1
-    datamodule.upsample_stride = 2
+# print(type(datamodule))
+# print(len(train_loader))
+datamodule.square_edge = 513
+datamodule.extended_scale = False
+datamodule.orientation_invariant = 0.1
+datamodule.upsample_stride = 2
+
+print('square_edge: ', datamodule.square_edge)
+print('extended_scale: ', datamodule.extended_scale)
+print('orientation_invariant: ', datamodule.orientation_invariant)
+print('upsample_stride: ', datamodule.upsample_stride)
 
 
-    train_loader = datamodule.vis_train_loader()
-    val_loader = datamodule.vis_val_loader()
+train_loader = datamodule.vis_train_loader()
+val_loader = datamodule.vis_val_loader()
 
-    train_instance_scales = []
-    val_instance_scales = []
-
-    
-
-    for data in train_loader:
-        print(type(data))
-        print(data)
-        # for ann in anns:
-        #     train_instance_scales.append(ann['bbox'][2] * ann['bbox'][3])
-
-    for batch, (image, anns, _) in enumerate(val_loader):
-        for ann in anns:
-            val_instance_scales.append(ann['bbox'][2] * ann['bbox'][3])
-
-    #draw a histogram of train_instance_scales
-    plt.hist(train_instance_scales, bins=50)
-    plt.title("COCOKP Train Instance Scales")
-    # plt.show()
-    #save the histogram
-    plt.savefig('/scratch/izar/jiguo/visualization/train_instance_scales.png')
-
-    #draw a histogram of val_instance_scales
-    plt.hist(val_instance_scales, bins=50)
-    plt.title("COCOKP Val Instance Scales")
-    # plt.show()
-    #save the histogram
-    plt.savefig('/scratch/izar/jiguo/visualization/val_instance_scales.png')
+train_instance_scales = []
+val_instance_scales = []
 
 
 
-if __name__ == '__main__':
-    main()
+for batch, (image, anns, _) in enumerate(train_loader):
+    # print(type(data))
+    # print(data)
+    for ann in anns:
+        train_instance_scales.append(ann['bbox'][2] * ann['bbox'][3])
+
+for batch, (image, anns, _) in enumerate(val_loader):
+    for ann in anns:
+        val_instance_scales.append(ann['bbox'][2] * ann['bbox'][3])
+
+#draw a histogram of train_instance_scales
+plt.hist(train_instance_scales, bins=50)
+plt.title("COCOKP Train Instance Scales")
+# plt.show()
+#save the histogram
+plt.savefig('/scratch/izar/jiguo/visualization/train_instance_scales.png')
+
+#draw a histogram of val_instance_scales
+plt.hist(val_instance_scales, bins=50)
+plt.title("COCOKP Val Instance Scales")
+# plt.show()
+#save the histogram
+plt.savefig('/scratch/izar/jiguo/visualization/val_instance_scales.png')
+
+
+
+# if __name__ == '__main__':
+#     main()
