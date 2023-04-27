00873f12db14d889521a6d9034a05d54cc365313
The-Hobbit-G <guojianting0616@163.com>
2023-04-27 22:45:48 +0200

    free more memory

--- src/openpifpaf/transforms/compose.py
+++ src/openpifpaf/transforms/compose.py
@@ -9,6 +9,44 @@ class Compose(Preprocess):
         self.preprocess_list = preprocess_list
 
     def __call__(self, *args):
+        anns = []
+        for p in self.preprocess_list:
+            if p is None:
+                continue
+            if (self.preprocess_list.index(p) == len(self.preprocess_list)-1) and (type(p) == list):
+                images = None
+                meta = None
+                for p_single in p:
+                    result = p_single(*args)
+                    if images is None:
+                        images = result[0]
+                    if meta is None:
+                        meta = result[2]
+                    anns.append(result[1])
+                args = images, anns, meta #images will be the same as before, anns and meta would be type<list>
+                del images, anns, meta, result
+            else:
+                args = p(*args)
+
+        return args
+
+    #enable us to further manipulate the preprocess compose after construction
+    def pop(self, position):
+        return self.preprocess_list.pop(position)
+
+    def append(self, process):
+        self.preprocess_list.append(process)
+
+
+
+'''
+#last version
+class Compose(Preprocess):
+    """Execute given transforms in sequential order."""
+    def __init__(self, preprocess_list: List[Preprocess]):
+        self.preprocess_list = preprocess_list
+
+    def __call__(self, *args):
         for p in self.preprocess_list:
             if p is None:
                 continue
@@ -36,6 +74,7 @@ class Compose(Preprocess):
 
     def append(self, process):
         self.preprocess_list.append(process)
+'''
 
 
 
