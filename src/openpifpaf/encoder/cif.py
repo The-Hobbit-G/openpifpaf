4d3af835ce1e602d64ed1dd94e140bf1171ac13e
The-Hobbit-G <guojianting0616@163.com>
2023-07-28 12:03:00 +0800

    visualize gt

--- src/openpifpaf/encoder/cif.py
+++ src/openpifpaf/encoder/cif.py
@@ -85,18 +85,19 @@ class CifGenerator():
             #fields would be in the order of [center,top_left,bottom_right], and there would be n_categories of them
 
             #TODO: visualize the fields
-            # print(type(image))
-            # for detection in detections:
-            #     center_point = detection[1][:2]
-            #     top_left_point = detection[1][2:4]
-            #     bottom_right_point = detection[1][4:]
-            #     #draw center point in image with red color using opencv
-            #     image = cv2.circle(image, (int(center_point[0]), int(center_point[1])), 5, (0,0,255), -1)
-            #     #draw top left point in image with green color using opencv
-            #     image = cv2.circle(image, (int(top_left_point[0]), int(top_left_point[1])), 5, (0,255,0), -1)
-            #     #draw bottom right point in image with blue color using opencv
-            #     image = cv2.circle(image, (int(bottom_right_point[0]), int(bottom_right_point[1])), 5, (255,0,0), -1)
-            # cv2.imwrite('/home/jiguo/test.jpg', image)
+            # print(type(image)) #<class 'torch.tensor'>
+            image = image.numpy()
+            for detection in detections:
+                center_point = detection[1][:2]
+                top_left_point = detection[1][2:4]
+                bottom_right_point = detection[1][4:]
+                #draw center point in image with red color using opencv
+                image = cv2.circle(image, (int(center_point[0]), int(center_point[1])), 5, (0,0,255), -1)
+                #draw top left point in image with green color using opencv
+                image = cv2.circle(image, (int(top_left_point[0]), int(top_left_point[1])), 5, (0,255,0), -1)
+                #draw bottom right point in image with blue color using opencv
+                image = cv2.circle(image, (int(bottom_right_point[0]), int(bottom_right_point[1])), 5, (255,0,0), -1)
+            cv2.imwrite('/home/jiguo/test.jpg', image)
 
 
         return fields
