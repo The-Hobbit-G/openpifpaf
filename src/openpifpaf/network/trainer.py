38babbaa7fd6ce309493b66640c1ca8313ebcedc
jianting guo <guojianting0616@163.com>
2023-09-21 15:00:22 +0200

    remove print

--- src/openpifpaf/network/trainer.py
+++ src/openpifpaf/network/trainer.py
@@ -187,9 +187,9 @@ class Trainer():
                         assert target.is_pinned()
 
             # print(type(targets),type(targets[0]),type(targets[1]),type(targets[2]))
-            print(type(targets),type(targets[0]),type(targets[1]))
-            print(len(targets),len(targets[0]),len(targets[1]))
-            print(type(data),len(data))
+            # print(type(targets),type(targets[0]),type(targets[1]))
+            # print(len(targets),len(targets[0]),len(targets[1]))
+            # print(type(data),len(data))
             
             #for FPN case
             if type(targets[0]) == list and ((isinstance(self.model,nets.Shell) and self.model.neck_net is not None) or \
