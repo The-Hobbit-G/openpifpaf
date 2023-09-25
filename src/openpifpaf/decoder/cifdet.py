ebd8928a97cfe6d9c68de1779dc357e7bf90ae21
jianting guo <guojianting0616@163.com>
2023-09-26 01:25:31 +0200

    check cifdet decoder shape

--- src/openpifpaf/decoder/cifdet.py
+++ src/openpifpaf/decoder/cifdet.py
@@ -74,7 +74,8 @@ class CifDet(Decoder):
                   (time.perf_counter() - start) * 1000.0)
 
         print(categories, scores, boxes)
-        print(type(categories), type(scores), type(boxes))
+        # print(type(categories), type(scores), type(boxes)) #all torch.Tensor
+        print(categories.shape, scores.shape, boxes.shape) #(n,) (n,) (n,4
 
         # convert to py
         annotations_py = []
