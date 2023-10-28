89ed3bfd4bef0c28f03d36ce1af99c121c7a1889
jianting guo <guojianting0616@163.com>
2023-10-28 16:33:18 +0200

    correct the evalxp name

--- script_examples/opp_val_resnet50_cifcafdet_cocodet.sh
+++ script_examples/opp_val_resnet50_cifcafdet_cocodet.sh
@@ -7,7 +7,7 @@ shopt -s extglob
 echo STARTING AT `date`
 
 
-evalxp='opp_val_resnet50_cifcafdet_bg_mask_det_cocodet_sigma0.8'
+evalxp='opp_train_resnet50_cifcafdet_cocodet_bg_mask_det_sigma0.8'
 evalepoch=018
 
 
